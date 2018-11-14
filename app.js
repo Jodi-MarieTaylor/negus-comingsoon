9437ca1da9339bd0d1d2fe8df042e2dda30e5084
Jodi-Marie <jodimarietaylor18@gmail.com>
2018-11-13 22:19:05 -0500

    coming soon

---
+++ app.js
@@ -0,0 +1,23 @@
+
+var express  = require("express"),
+    app  = express(),
+    ejs = require('ejs'),
+
+    http = require("http").Server(app);
+
+app.use(express.json());
+//app.use("/css", express.static("./css"))
+//app.use("/js", express.static("./js"))
+app.use("/images", express.static("./images"))
+//app.use("/js", express.static(__dirname + '/js'));
+//console.log(express.static('./js'))
+
+
+console.log("Server started");
+app.get("/", function(req, res) {
+ res.sendFile(__dirname + "/index.html")
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Listening on port ${port}'));
