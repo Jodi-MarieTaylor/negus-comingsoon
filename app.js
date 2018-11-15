93a578176bda43bdd6ee8837bfa6e8865cd18418
Jodi-Marie <jodimarietaylor18@gmail.com>
2018-11-14 22:44:12 -0500

    animated gif

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express  = require("express"),
     http = require("http").Server(app);
 
 app.use(express.json());
-//app.use("/css", express.static("./css"))
+app.use("/css", express.static("./css"))
 //app.use("/js", express.static("./js"))
 app.use("/images", express.static("./images"))
 //app.use("/js", express.static(__dirname + '/js'));
